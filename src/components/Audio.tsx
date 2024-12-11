1a77501c82a53c97ae7f7a70f288543a60cf15ac
yama4936 <t323088@m.chukyo-u.ac.jp>
2024-12-11 18:03:31 +0900

    ボタンなしで音声が流れるように変更

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -1,14 +1,13 @@
+import React from 'react';
 import useSound from 'use-sound';
 import Sound from "../assets/audio/bell.mp3";
 
 export const Audio: React.FC = () => {
-  const [play, { stop, pause }] = useSound(Sound);
+  const [play] = useSound(Sound);
 
-  return (
-    <>
-      <button onClick={() => play()}>音を鳴らす</button>
-      <button onClick={() => stop()}>停止</button>
-      <button onClick={() => pause()}>ポーズ</button>
-    </>
-  );
+  React.useEffect(() => {
+    play();
+  }, [play]);
+
+  return null;
 };
